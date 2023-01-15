19c57c1de8340a1c47c3ffbf4a046a6b93f64cec
Millie Muteti <milliemuteti@gmail.com>
2023-01-15 23:09:41 +0300

    4. Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
 
+"""
+starts a flask web application
+"""
+
 from flask import Flask
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+@app.route('/')
+def index():
+    """ returns a message"""
+    return ("Hello HBNB!")
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    return "HBNB"
+    """new path"""
+    return ("HBNB")
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cfun(text):
-    txt = text.replace('_', " ")
-    return "C {}". format(txt)
+@app.route('/c/<text>')
+def c(text):
+    """ display C with value in variable text """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    txt = text.replace('_', " ")
-    return "Python {}". format(txt)
+@app.route('/python')
+@app.route('/python/<text>')
+def pythoncool(text='is cool'):
+    """ display python with value of text variable """
+    return "Python " + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return '{} is a number'.format(n)
+@app.route('/number/<int:n>')
+def numb(n):
+    """ display python with value of text variable """
+    return '{:d} is a number'.format(n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
